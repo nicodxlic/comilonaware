b3388c37aa13188ad71aea0a1e85dd3ce882348e
elpista <espadachin2003@gmail.com>
2024-09-20 18:15:45 -0300

    get pedido y post pedido con relaciones hecha

--- comilonawareback/app/Http/Controllers/PedidoController.php
+++ comilonawareback/app/Http/Controllers/PedidoController.php
@@ -8,10 +8,7 @@ use Illuminate\Http\Request;
 class PedidoController extends Controller
 {
     public function index(){
-        $pedidos = [
-            (object) ['mesa' => 3, 'id' => 1, 'precio' => 20.5, 'estado' => 'pendiente', 'id_usuario' => 1],
-            (object) ['nombre' => 'milanesa', 'id' => 2, 'precio' => 15.6, 'estado' => 'pendiente', 'id_usuario' => 1]
-        ];
+        $pedidos = Pedido::all();
 
         return $pedidos;
     }
@@ -19,12 +16,14 @@ class PedidoController extends Controller
     public function crear(Request $request){
         if ($request) {
 
-        Pedido::create([
+        $pedido = Pedido::create([
             'mesa' => $request->input('mesa'),
             'estado' => $request->input('estado'),
             'precio' => $request->input('precio'),
             //'id_usuario' => Auth::id(),
         ]);
+
+        $pedido->productos()->attach($request->input('productos'));
 
         return 'pedido creado con éxito';
     } else {
