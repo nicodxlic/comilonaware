218ca795c234cc2beed66cae3601d4a660fce945
Nicolas <axel.maldonado@est.fi.uncoma.edu.ar>
2024-09-22 18:10:11 -0300

    Lista de productos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { ROUTES } from './const/routes.js';
+import { BrowserRouter, Route, Routes, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from './pages/Home.jsx';
+import Products from './components/Product/Products.jsx';
+
+
+
+/*const router = createBrowserRouter([
+  {
+    path: ROUTES.home,
+    element: <Home />,
+  },{
+    path: ROUTES.order,
+    element: <Order />
+  }
+]); */
+
+// <RouterProvider router = {router} />
+
+
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Products/> }/>
+      </Routes>
+      </BrowserRouter>
       <header className="App-header">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button className="btn btn-primary">Crear</button>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ frontend/src/components/Product/Products.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import {Link} from 'react-router-dom'
+
+/**Hice una prueba para ver si traia los items de la BD
+ * Posiblemente la funcionalidad de este componente cambie
+ */
+const endpoint = 'http://localhost:8000/api'
+
+const Products = () => {
+
+    const [products, setProducts] = useState([])
+    useEffect ( () => {
+        getAllProducts()
+    }, [])
+
+    const getAllProducts = async () => {
+       const response = await axios.get(`${endpoint}/products`)
+       setProducts(response.data)
+    }
+
+    const deleteProduct = async (id) => {
+      axios.delete(`${endpoint}/product/${id}`)
+      getAllProducts()
+    }
+
+  return (
+    <div>
+    <table className='table table-striped'>
+      <thead className='bg-primary text-white text-center'>
+        <tr>
+          <th>Nombre</th>
+          <th>Precio</th>
+          <th>Stock</th>
+        </tr>
+      </thead>
+      <tbody className='text-center'>
+        { products.map ((product) => (
+          <tr key={product.id}>
+            <td> {product.name} </td>
+            <td> {product.price} </td>
+            <td> {product.stock} </td>
+            <td>
+              <Link to={`/edit/${product.id}`} className='btn btn-warning'>Editar</Link>
+              <button onClick={ ()=> deleteProduct(product.id) } className='btn btn-danger'>Eliminar</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    <div className='d-grip gap-2'>
+      <Link to="/create" className='btn btn-success mt-2 ml-2 text-white'>Agregar nuevo producto</Link>
+    </div>
+    </div>
+  )
+}
+
+export default Products
