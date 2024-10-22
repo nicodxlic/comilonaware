e298cb4524a25f1d98a2049bd78286fcdee4f42b
elpista <espadachin2003@gmail.com>
2024-10-22 15:54:26 -0300

    ver los pedidos de una compra

--- frontend/src/components/Purchase/PurchaseList.jsx
+++ frontend/src/components/Purchase/PurchaseList.jsx
@@ -1,19 +1,49 @@
 import React, { useEffect, useState } from 'react'
 import {axios} from '../../axiosConfig.js'
+import Swal from "sweetalert2"
 
 const endpoint = 'http://localhost:8000/api'
 
 const PurchaseList = () => {
     const [purchases, setPurchases] = useState([])
+    const [ordersPurchase, setOrdersPurchase] = useState([])
 
     const getAllPurchases = async () => {
         const response = await axios.get(`${endpoint}/purchases`)
-        console.log(response.data[0])
         setPurchases(response.data)
     }
 
-    const getOrders = async (id) => {
-        console.log('no hago nada')
+    const showOrders = async (id) => {
+        Swal.fire({
+            title: 'Cargando...',
+            text: 'Por favor espera',
+            allowOutsideClick: false, // Evita que el usuario cierre el loader clickeando fuera del modal
+            didOpen: () => {
+              Swal.showLoading(); // Activa el spinner
+            }
+          })
+          let response = await axios.get(`${endpoint}/order/purchase/${id}`);
+          let orders = response.data; // Usa directamente la respuesta de axios
+          
+          let HTML = '';
+          
+          for(let i = 0; i < orders.length; i++){
+              HTML = HTML + 'Pedido ID: ' + orders[i].id + '<br/>';
+              
+              let responseOrderProducts = await axios.get(`${endpoint}/orders_products/` + orders[i].id);
+              
+              for(let j = 0; j < responseOrderProducts.data.length; j++){
+                  let productResponse = await axios.get(`${endpoint}/product/${responseOrderProducts.data[j].product_id}`);
+                  HTML = HTML + productResponse.data.name + '<br/>';
+              }
+          }
+        Swal.close()
+        Swal.fire({
+          title: 'Pedidos de la compra',
+          html: HTML,
+          icon: 'info',
+          confirmButtonText: 'Cerrar'
+        });
     }
 
     useEffect ( () => {
@@ -42,7 +72,7 @@ const PurchaseList = () => {
                         <td> {purchase.changePay} </td>
                         <td> {purchase.payMethod} </td>
                         <td>
-                        <button type="button" onClick={() => getOrders(purchase.id)} className='btn btn-info'>Ver pedidos</button>
+                        <button type="button" onClick={() => showOrders(purchase.id)} className='btn btn-info'>Ver pedidos</button>
                         </td>
                     </tr>
                     ))}
