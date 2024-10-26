a123b9b32787356cfed7d8064bbf83225294a226
elpista <espadachin2003@gmail.com>
2024-10-26 17:11:23 -0300

    filtrado por categorias en products

--- frontend/src/components/Product/Products.jsx
+++ frontend/src/components/Product/Products.jsx
@@ -1,86 +1,120 @@
 import React, { useEffect, useState } from 'react'
 import {axios} from '../../axiosConfig.js'
-
 import {Link} from 'react-router-dom'
 import ProductsCard from './ProductsCard.jsx'
-
+import Swal from "sweetalert2"
 
 const endpoint = 'http://localhost:8000/api'
 
 const Products = (role) => {
-
+    const [categories, setCategories] = useState([])
     const [products, setProducts] = useState([])
-    const [filteredProducts, setFilteredProducts] = useState([])
+    const [shownProducts, setShownProducts] = useState([])
     const [searchTerm, setSearchTerm] = useState('')
+    const [selectedCategory, setSelectedCategory] = useState('')
     const [productMesage, setProductMesage] = useState('Cargando productos...')
 
     const getAllProducts = async () => {
-        const response = await axios.get(`${endpoint}/products`)
-        if(role.role == 'admin'){
+        Swal.fire({
+            title: 'Cargando...',
+            text: 'Por favor espera',
+            allowOutsideClick: false,
+            didOpen: () => {
+              Swal.showLoading();
+            }
+          })
+        let response = await axios.get(`${endpoint}/products`)
+        
+        if(role.role === 'admin'){
             if(response.data.length === 0){
                 setProductMesage('No se han encontrado productos')
             } else {
                 setProducts(response.data)
-                setFilteredProducts(response.data)
+                setShownProducts(response.data) // Inicializar shownProducts
             }
         } else {
             let sortedOrders = response.data.filter(product => product.deleted !== 1)
-        if(sortedOrders.length === 0){
-            setProductMesage('No se han encontrado productos')
-        } else{
-            sortedOrders = sortedOrders.sort((a, b) => {
-            return a.enabled === b.enabled ? 0 : a.enabled ? -1 : 1;
-        });
-
-        setProducts(sortedOrders)
-        setFilteredProducts(sortedOrders)
-       }
+            if(sortedOrders.length === 0){
+                setProductMesage('No se han encontrado productos')
+            } else{
+                sortedOrders = sortedOrders.sort((a, b) => {
+                    return a.enabled === b.enabled ? 0 : a.enabled ? -1 : 1;
+                });
+                response = await axios.get(`${endpoint}/categories`)
+                setCategories(response.data)
+                setProducts(sortedOrders)
+                setShownProducts(sortedOrders) // Inicializar shownProducts
+                Swal.close()
+           }
        }
     }    
     
-    useEffect ( () => {
+    useEffect(() => {
         getAllProducts()
     }, [])
 
-  useEffect(() => {
-    const filtered = products.filter(product =>
-        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    useEffect(() => {
+        // Filtrar productos según el término de búsqueda y la categoría seleccionada
+        const filtered = products.filter(product =>
+            product.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
+            (selectedCategory === '' || product.category_id === parseInt(selectedCategory))
+        )
+        setShownProducts(filtered)
+    }, [searchTerm, selectedCategory, products])
+
+    const handleCategoryChange = (e) => {
+        setSelectedCategory(e.target.value) // Actualiza la categoría seleccionada
+    }
+
+    return (
+        <div className="container mx-auto p-4">
+            <h3 className="text-2xl font-bold mb-4">Productos</h3>
+            <div className='flex justify-center mt-6 mb-6'>
+                <Link to="/create-product" className='bg-green-500 text-white px-4 py-2 rounded-lg'>
+                    Agregar nuevo producto
+                </Link>
+            </div>
+            <div className="mb-6 flex justify-start items-center space-x-4">
+                <input 
+                    type='text' 
+                    placeholder='Buscar productos'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    className="w-1/3 p-3 border border-gray-300 rounded-md"
+                />
+                <div className="flex items-center pl-12">
+                    <p className="mr-2">Filtrar categorías:</p>
+                    <select 
+                        onChange={(e) => handleCategoryChange(e)}
+                        className="p-2 border border-gray-300 rounded-md"
+                    >
+                        <option value="">Seleccionar una categoria</option>
+                        {categories.map((category, index) => (
+                            <option key={index} value={category.id}>
+                                {category.name}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            </div>
+
+            {/* Grid layout para las cards */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {shownProducts.length > 0 ? (
+                    shownProducts.map(product => (
+                        <ProductsCard 
+                            key={product.id}
+                            product={product}
+                            getAllProducts={getAllProducts}
+                            categories={categories}
+                        />
+                    ))
+                ) : (
+                    <p className="text-center col-span-full">{productMesage}</p>
+                )}
+            </div>
+        </div>
     )
-    setFilteredProducts(filtered)
-  }, [searchTerm, products])
-
-  return (
-    <div className="container mx-auto p-4">
-        <h3 className="text-2xl font-bold mb-4">Productos</h3>
-        <div className='flex justify-center mt-6 mb-6'>
-            <Link to="/create-product" className='bg-green-500 text-white px-4 py-2 rounded-lg'>
-                Agregar nuevo producto
-            </Link>
-        </div>
-        <div className="mb-6">
-            <input 
-                type='text' 
-                placeholder='Buscar productos'
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                className="w-full p-2 border border-gray-300 rounded-lg"
-            />
-        </div>
-
-        {/* Grid layout para las cards */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {filteredProducts.length > 0 ? (
-                filteredProducts.map(product => (
-                    <ProductsCard 
-                    product = {product}
-                    getAllProducts={getAllProducts}/>
-                ))
-            ) : (
-                <p className="text-center col-span-full">{productMesage}</p>
-            )}
-        </div>
-    </div>
-)
 }
 
 export default Products
--- frontend/src/components/Product/ProductsCard.jsx
+++ frontend/src/components/Product/ProductsCard.jsx
@@ -4,7 +4,7 @@ import Swal from "sweetalert2"
 
 const endpoint = 'http://localhost:8000/api'
 
-const ProductsCard = ({product, getAllProducts}) => {
+const ProductsCard = ({product, getAllProducts, categories}) => {
 
   const deleteProduct = async (id) => {
     Swal.fire({
@@ -15,7 +15,7 @@ const ProductsCard = ({product, getAllProducts}) => {
         Swal.showLoading(); // Activa el spinner
       }
     })
-    const response = await axios.put(`${endpoint}/product/delete/` + id)
+    await axios.put(`${endpoint}/product/delete/` + id)
     Swal.close()
     Swal.fire({
         icon: 'success',
@@ -27,78 +27,86 @@ const ProductsCard = ({product, getAllProducts}) => {
   const editProduct = async (product) => {
     // Mostrar el formulario con los valores actuales del producto
     const { value: formValues } = await Swal.fire({
-        title: 'Editar producto',
-        html: `
-            <div style="position: relative;">
-                <button id="close-btn" style="position: absolute; top: 0; right: 0; background: none; border: none; font-size: 18px; cursor: pointer;">&times;</button>
-            </div>
-            <div style="display: flex; flex-direction: column;">
-                <label for="swal-input1">Nombre del producto:</label>
-                <input id="swal-input1" class="swal2-input" placeholder="Nombre del producto" value="${product?.name || ''}">
-
-                <label for="swal-input2" style="margin-top: 10px;">Precio:</label>
-                <input id="swal-input2" class="swal2-input" type="number" placeholder="Precio" value="${product?.price || ''}">
-
-                <label for="swal-input3" style="margin-top: 10px;">URL de la imagen:</label>
-                <input id="swal-input3" class="swal2-input" type="text" placeholder="URL de la imagen" value="${product?.image || ''}">
-            </div>
-        `,
-        showCancelButton: true,
-        cancelButtonText: 'Cancelar',
-        focusConfirm: false,
-        preConfirm: () => {
-            const name = document.getElementById('swal-input1').value;
-            const price = document.getElementById('swal-input2').value;
-            const image = document.getElementById('swal-input3').value; // URL de la imagen
-
-            // Validación de los campos
-            if (!name || !price || !image) {
-                Swal.showValidationMessage('Todos los campos son obligatorios');
-                return false; // Prevenir el cierre del modal si la validación falla
-            }
-
-            return { name, price, image };
-        },
-        didOpen: () => {
-            // Añadir evento para cerrar con la "X"
-            document.getElementById('close-btn').addEventListener('click', () => {
-                Swal.close();
-            });
-        }
+      title: 'Editar producto',
+      html: `
+          <div style="position: relative;">
+              <button id="close-btn" style="position: absolute; top: 0; right: 0; background: none; border: none; font-size: 18px; cursor: pointer;">&times;</button>
+          </div>
+          <div style="display: flex; flex-direction: column;">
+              <label for="swal-input1">Nombre del producto:</label>
+              <input id="swal-input1" class="swal2-input" placeholder="Nombre del producto" value="${product?.name || ''}">
+              <label for="swal-input2" style="margin-top: 10px;">Precio:</label>
+              <input id="swal-input2" class="swal2-input" type="number" placeholder="Precio" value="${product?.price || ''}">
+              <label for="swal-input3" style="margin-top: 10px;">URL de la imagen:</label>
+              <input id="swal-input3" class="swal2-input" type="text" placeholder="URL de la imagen" value="${product?.image || ''}">
+              <label for="swal-select" style="margin-top: 10px;">Categoría:</label>
+              <select id="swal-select" class="swal2-input">
+                  ${categories.map(category => `
+                      <option value="${category.id}" ${category.id === product?.category_id ? 'selected' : ''}>
+                          ${category.name}
+                      </option>
+                  `).join('')}
+              </select>
+          </div>
+      `,
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar',
+      focusConfirm: false,
+      preConfirm: () => {
+          const name = document.getElementById('swal-input1').value;
+          const price = document.getElementById('swal-input2').value;
+          const image = document.getElementById('swal-input3').value; // URL de la imagen
+          const category_id = document.getElementById('swal-select').value;
+          // Validación de los campos
+          if (!name || !price || !image || !category_id) {
+              Swal.showValidationMessage('Todos los campos son obligatorios');
+              return false; // Prevenir el cierre del modal si la validación falla
+          }
+          return { name, price, image, category_id };
+      },
+      didOpen: () => {
+          // Añadir evento para cerrar con la "X"
+          document.getElementById('close-btn').addEventListener('click', () => {
+              Swal.close();
+          });
+      }
     });
 
     // Si el usuario confirmó y el formulario es válido, puedes manejar los datos actualizados
     if (formValues) {
-        const updatedProduct = {
-            ...product,
-            name: formValues.name,
-            price: formValues.price,
-            image: formValues.image, // URL de la imagen
-        };
-        Swal.fire({
+      const updatedProduct = {
+          ...product,
+          name: formValues.name,
+          price: formValues.price,
+          image: formValues.image, // URL de la imagen
+          category_id: formValues.category_id, // Categoría seleccionada
+      };
+      Swal.fire({
           title: 'Cargando...',
           text: 'Por favor espera',
           allowOutsideClick: false, // Evita que el usuario cierre el loader clickeando fuera del modal
           didOpen: () => {
-            Swal.showLoading(); // Activa el spinner
+              Swal.showLoading(); // Activa el spinner
           }
-        })
-        await axios.get('/sanctum/csrf-cookie');
-        let response = await axios.put(`${endpoint}/product/update/` + product.id, {
+      });
+      await axios.get('/sanctum/csrf-cookie');
+      await axios.put(`${endpoint}/product/update/` + product.id, {
           name: updatedProduct.name, 
           price: updatedProduct.price, 
           image: updatedProduct.image, 
+          category_id: updatedProduct.category_id,  // Actualiza la categoría
           deleted: updatedProduct.deleted, 
           enabled: updatedProduct.enabled
-        });
-        getAllProducts()
-        Swal.close()
-        Swal.fire({
-            icon: 'success',
-            title: 'Producto actualizado correctamente',
-        });
+      });
+      getAllProducts();
+      Swal.close();
+      Swal.fire({
+          icon: 'success',
+          title: 'Producto actualizado correctamente',
+      });
     }
-};
+  };
+
   const disableProduct = async (id) => {
     Swal.fire({
       title: 'Cargando...',
@@ -112,7 +120,7 @@ const ProductsCard = ({product, getAllProducts}) => {
     const response = await axios.patch(`${endpoint}/product/disable/` + id)
     getAllProducts()
     Swal.close()
-    if(response.data.enabled == 0){
+    if(response.data.enabled === 0){
         Swal.fire({
             icon: 'success',
             title: '¡Éxito!',
@@ -126,7 +134,7 @@ const ProductsCard = ({product, getAllProducts}) => {
         })
     }
   }
-  if (product.enabled == 0 && product.deleted == 0) {
+  if (product.enabled === 0 && product.deleted === 0) {
       return (
         <div key={product.id} className="bg-red-500 shadow-lg rounded-lg p-4">
           <p className="text-yellow-300 font-bold mb-4">Deshabilitado</p>
@@ -158,7 +166,7 @@ const ProductsCard = ({product, getAllProducts}) => {
           </button>
         </div>
       );
-  } else if (product.enabled == 1 && product.deleted == 0) {
+  } else if (product.enabled === 1 && product.deleted === 0) {
     return (
       <div key={product.id} className="bg-white shadow-lg rounded-lg p-4">
         <h4 className="font-bold text-lg mb-2">{product.name}</h4>
